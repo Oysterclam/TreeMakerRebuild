c55b1a56c8acf6aaaac98b3e70b22ebb11e2b1e5
Andy J Yang <jandy@uchicago.edu>
2020-05-25 10:46:09 -0700

    initial

---
+++ mac/build/wxMac-2.6/build-release/.deps/monolib_stringops.o.d
@@ -0,0 +1,12 @@
+monolib_stringops.o: ../src/common/stringops.cpp ../include/wx/wxprec.h \
+  ../include/wx/defs.h ../include/wx/platform.h ../include/wx/compiler.h \
+  /Users/andyyang/Desktop/TreeMaker/mac/build/wxMac-2.6/build-release/lib/wx/include/osx_cocoa-unicode-static-3.1/wx/setup.h \
+  ../include/wx/chkconf.h ../include/wx/osx/chkconf.h \
+  ../include/wx/osx/cocoa/chkconf.h ../include/wx/unix/chkconf.h \
+  ../include/wx/version.h ../include/wx/cpp.h ../include/wx/dlimpexp.h \
+  ../include/wx/types.h ../include/wx/debug.h ../include/wx/chartype.h \
+  ../include/wx/windowid.h ../include/wx/features.h \
+  ../include/wx/stringops.h ../include/wx/stringimpl.h \
+  ../include/wx/wxcrtbase.h ../include/wx/beforestd.h \
+  ../include/wx/afterstd.h ../include/wx/unichar.h \
+  ../include/wx/buffer.h
